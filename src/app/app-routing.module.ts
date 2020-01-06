85937955634f2a38e880a6b5b3e81eac7f91bf21
Nobleson <noblesity@gmail.com.com>
2020-01-06 19:02:33 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,9 @@ const routes: Routes = [
   { path: '', redirectTo: 'tabs', pathMatch: 'full' },
   { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
   { path: 'hymn0001', loadChildren: './pages/hymn0001/hymn0001.module#Hymn0001PageModule' },
+  { path: 'setting', loadChildren: './pages/setting/setting.module#SettingPageModule' },
+  { path: 'help', loadChildren: './pages/help/help.module#HelpPageModule' },
+  { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
 
 ];
 
